d33e621b2573c24d93ce683e9177db2396624074
Luis Alfredo Vejo Mendoza <69359637+LuisAlfredoVejo@users.noreply.github.com>
2022-02-16 12:19:12 -0600

    Menu recursivo creado

--- T2_201212527.cpp
+++ T2_201212527.cpp
@@ -15,44 +15,68 @@ typedef struct mbr
 };
 
 
-void exec (string entrada){
+void exec (){
     
-    cout<<"Comando exec"<<endl;
+    cout<<"Comando EXEC"<<endl;
 
+}
+
+void mkdisk(){
+
+    cout<<"Comando MKDISK"<<endl;
+}
+
+void rep(){
+
+    cout<<"Comando REP"<<endl;
 }
 
 void salir(){
 
+    cout<<"Se cerrara la aplicacion"<<endl;
     exit(0);
 
 }
 
+void menu(){
+    cout<<"Ingrese un comando:  "<<endl;
+    cin>>entrada;
+    
+}
+
 void analizador(string entrada){
 
-    if(entrada == "salir")
+    if(entrada == "exec")
     {
+        exec();       
+    }
+    else if(entrada == "mkdisk"){
+        mkdisk();
+    }
+    else if(entrada == "rep"){
+        rep();
+    }
+    else if(entrada == "salir"){
         salir();
     }
-    else if (entrada == "exec")
-    {
-        exec(ruta);
+    else{
+        cout<<"Comando incorrecto"<<endl;
     }
-    
-    else
-    {
-        cout<<"Comando Incorrecto"<<endl;
-    }
-
 }
+
 
 int main()
 {
     cout<<"Tarea No.2 - Manejo E Implementacion de Archivos"<<endl;
     cout<<"Luis Alfredo Vejo Mendoza - 201212527"<<endl;
-    cout<<"Ingrese un comando:  "<<endl;
-    cin>>entrada;
-    analizador(entrada);
-
+    
+    do
+    {
+        menu();
+        analizador(entrada);
+    } while (entrada != "salir");
+    
+    
     
        
 }
